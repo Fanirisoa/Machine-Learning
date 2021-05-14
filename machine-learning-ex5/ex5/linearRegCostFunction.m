3ae90f8dd6e57c6986f993ee9f4cae77a7a9d011
Fanirisoa Zazaravaka <fanirisoa.zazaravaka@gmail.com>
2021-05-14 20:02:04 +0200

    update new version

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -19,6 +19,11 @@ m = length(y);
 %               You should set J to the cost and grad to the gradient.
 %
 
+
+fprintf(['size(theta): '], size(theta));
+
+
+
 transpose_theta = transpose(theta);
 h_theta = sigmoid(transpose(theta)*transpose(X));
 inter_val = ((h_theta - transpose(y))*(h_theta - transpose(y)))/(2*m)
