c8a994dbc25cd9a8026157885643ba43fa8fe610
Fanirisoa Zazaravaka <fanirisoa.zazaravaka@gmail.com>
2021-05-19 01:31:22 +0200

    update new version

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -43,16 +43,18 @@ Theta_grad = zeros(size(Theta));
 
 
 
+% calculating cost function.
+diff = (X*transpose(Theta)-Y);
+J = sum((diff.^2)(R==1))/2;
+J = J + lambda*sum(sum(Theta.^2))/2;  % regularized term of theta.
+J = J + lambda*sum(sum(X.^2))/2;     % regularized term of x.
+
+X_grad = (diff.*R)*Theta;                 %unregularized vectorized implementation
+Theta_grad = (transpose((diff.*R))*X);              %unregularized vectorized implementation
 
 
-
-
-
-
-
-
-
-
+X_grad = X_grad + (lambda * X);             % regularized
+Theta_grad = Theta_grad + (lambda * Theta);  % regularized
 
 
 % =============================================================
